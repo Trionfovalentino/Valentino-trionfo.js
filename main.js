5fe1c808f8c2318c331dba74e468fd145985021d
Trionfovalentino <trionfovalentino26@gmail.com>
2026-04-16 18:35:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 //stampo il valore di num1 e num2
-let num1 = 10;
-let num2 = 250;
+//let num1 = 10;
+//let num2 = 250;
 
-console.log(num1,num2);
+//console.log(num1,num2);
 
 //dichiaro la stringa 
-let nome='Marco';
+//let nome='Marco';
 
 //sostituisco la stringa 
-nome = '1';
+//nome = '1';
 
-console.log(nome);
+//console.log(nome);
 
 //dichiaro il PI
-const PI =  3.983274;
+//const PI =  3.983274;
 
-console.log(PI);
+//console.log(PI);
+
+
+//creo 5 variabili 
+
+var num1 = 29;
+var num2 = 209;
+var num3 = 299;
+var num4 = 321;
+var num5 = 8;
+
+var media = (num1 + num2 + num3 + num4 + num5) / 5;
+
+console.log("La somma tra i numeri equivale a", num1 + num2 + num3 + num4 + num5," e la media equivale a "  + media );
