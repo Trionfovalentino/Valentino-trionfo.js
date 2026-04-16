c309cb500946a35a2c0637fe4ade4d559c979364
Trionfovalentino <trionfovalentino26@gmail.com>
2026-04-16 18:36:21 +0200

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,11 @@
+//creo 5 variabili 
+
+var num1 = 29;
+var num2 = 209;
+var num3 = 299;
+var num4 = 321;
+var num5 = 8;
+
+var media = (num1 + num2 + num3 + num4 + num5) / 5;
+
+console.log("La somma tra i numeri equivale a", num1 + num2 + num3 + num4 + num5," e la media equivale a "  + media );
