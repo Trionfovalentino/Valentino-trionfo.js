2f105bd3ea0e2bfddc43d4684f656b0d1bdc645c
Trionfovalentino <trionfovalentino26@gmail.com>
2026-04-16 18:25:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//stampo il valore di num1 e num2
+let num1 = 10;
+let num2 = 250;
+
+console.log(num1,num2);
+
+//dichiaro la stringa 
+let nome='Marco';
+
+//sostituisco la stringa 
+nome = '1';
+
+console.log(nome);
+
+//dichiaro il PI
+const PI =  3.983274;
+
+console.log(PI);
